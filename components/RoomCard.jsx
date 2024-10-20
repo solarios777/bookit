9d8483473b597b4aa3384a8a33767e67cf4c494b
solomon <tsegasolomon635@gmail.com>
2024-10-20 11:20:13 +0300

    Room detail page and booking componens

--- components/RoomCard.jsx
+++ components/RoomCard.jsx
@@ -33,7 +33,7 @@ const RoomCard = ({room}) => {
       className="flex flex-col sm:flex-row w-full sm:w-auto sm:space-x-2 mt-2 sm:mt-0"
     >
       <Link
-        href={`/room/${room.$id}`}
+        href={`/rooms/${room.$id}`}
         className="bg-blue-500 text-white px-4 py-2 rounded mb-2 sm:mb-0 w-full sm:w-auto text-center hover:bg-blue-700"
         >View Room
         </Link >
