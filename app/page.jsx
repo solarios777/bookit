8d42a97a77e31733b687a6520626d3b168d633f5
solomon <tsegasolomon635@gmail.com>
2024-10-20 10:30:56 +0300

    RoomCard and Heading

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import rooms from "@/data/rooms.json"
+import RoomCard from "@/components/RoomCard";
+import Heading from "@/components/Heading";
 
 export default function Home() {
   return (
     <>
-    <h1>Bookit</h1>
+     <Heading title='Available Rooms' />
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} key={room.$id} />)
+      ) : (
+        <p>No rooms available at the moment</p>
+      )}
     </>
       
     
