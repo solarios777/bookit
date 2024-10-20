23db39abae3789178c24cdf07617ca243dabd4d0
solomon <tsegasolomon635@gmail.com>
2024-10-20 09:09:53 +0300

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import {Inter} from "next/font/google";
+import "@/assets/styles/globals.css";
+
+const inter= Inter({subsets:['latin']})
+
+export const metadata = {
+  title: "Bookit app",
+  description: "book a room for your team",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={inter.className}
+      >
+        <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        {children}
+
+        </main>
+      </body>
+    </html>
+  );
+}
